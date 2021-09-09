0341e1db6fbd0f6dd01919fbf89d6acf4b082408
Beautiful Code <sufares23@gmail.com>
2021-09-09 14:50:01 +0100

    +4

--- GitVoyage/GitVoyage.cs
+++ GitVoyage/GitVoyage.cs
@@ -9,6 +9,7 @@ namespace GitVoyage
  Console.WriteLine("Hello World!");
  Concole.WriteLine("Just added this from Visual Studio");
  Concole.WriteLine("Added to command line repo");
+ Console.WriteLine("I added this message to show the staging area");
  }
  }
 }
